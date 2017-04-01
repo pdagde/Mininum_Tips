7bad5503cf6a15c1f6c20de8f4f28d578ec21d96
pravin <pravin.dagde@medibox.in>
2017-04-01 15:58:29 +0530

    first commit

---
+++ Min_Tips.js
@@ -0,0 +1,56 @@
+function calculetTips(coinOne,coinTwo,totalBill){
+    var i=0;
+    var onetip=0;
+    while(coinOne+(coinTwo*i)<totalBill){	
+	i++; 
+    }
+    if(totalBill <= (coinOne+(coinTwo*i)) )
+    {
+	onetip = (coinOne+(coinTwo*i)) - totalBill;
+    }
+    return onetip;
+}
+
+
+function _minimumTip(coinOne,coinTwo,totalBill){
+    var i=0;
+    var minimumTip = coinOne; 
+    while((coinOne*i) <= totalBill)
+    {
+	var isminimumTips;
+	isminimumTips = calculetTips(coinOne*i,coinTwo,totalBill);
+	if(isminimumTips < minimumTip){
+	    minimumTip = isminimumTips;
+	}
+	i++;
+    }
+    if(calculetTips(coinOne*i,coinTwo,totalBill) < minimumTip)
+    {
+	minimumTip = isminimumTips;
+    }
+    return minimumTip;
+}
+
+function TASK_1(){
+
+    var BillNumber = 1;
+    if (process.argv.length < 3) {
+	console.log("PLEASE RUN# node Min_Tips.js input.txt");
+	process.exit(1);
+    }
+
+    var fs = require('fs');
+     filename = process.argv[2];
+    var lineReader = require('readline').createInterface({
+	input: require('fs').createReadStream(filename)
+    });
+    lineReader.on('line', function (data) {
+	var arr = data.split(" ");
+	arr = arr.map(function (val) { return +val;});
+	var minimumTip = _minimumTip(arr[0],arr[1],arr[2]);
+	console.log("Minimum tips BillNumber",BillNumber +"  is  "+ minimumTip);
+	BillNumber++;
+    });
+}
+
+TASK_1();
---
+++ README.md
@@ -0,0 +1,43 @@
+
+Question :
+
+             In Mars, there are only two denominations of currency, x and y.
+	     A Marsian goes to a bar and the bill is "z".
+	     Using x and y he has to pay the bill. But the bar doesnt tender change,
+	     any extra money will be taken as tips.
+	     helps the marsian to reduce the tips
+
+
+
+
+1) write input file.txt
+
+      | First_Coin |  second_coin | Total_Bill|
+      ----------------------------------------
+      | 2          | 5            | 109
+      | 5          | 7            | 43
+      | 15         | 19           | 33
+      | 5          | 6            | 10
+
+
+First_coin secound_coin Total_Bill  separeted with space
+
+
+
+2) run file using
+
+       node Min_Tips.js input.txt
+
+
+3) OUTPUT :
+
+           Minimum tips BillNumber 1  is  0
+	   Minimum tips BillNumber 2  is  0
+	   Minimum tips BillNumber 3  is  1
+	   Minimum tips BillNumber 4  is  0
+
+
+
+
+
+
